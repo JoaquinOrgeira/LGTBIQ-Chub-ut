e92846dc90f3f008bd29517f05d8d057ccad7cee
JoaquinOrgeira <137895977+JoaquinOrgeira@users.noreply.github.com>
2024-06-27 14:40:48 -0300

    Update funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -4,12 +4,12 @@ function listo(){
 	jQuery(".hamb").click(function(e){
     e.preventDefault();
     jQuery("header nav").toggleClass("open")
-    jQuery(".hamb i").toggleClass("fa-skull")
+    jQuery(".hamb i").toggleClass("")
 	});
 	jQuery("header nav a").click(function(){
 
     jQuery("header nav").removeClass("open")
-    jQuery(".hamb i").removeClass("fa-skull")	
+    jQuery(".hamb i").removeClass("")	
     var dev = jQuery(this).attr("href");
     jQuery("html,body").animate({
     	"scrollTop":jQuery(dev).offset().top
